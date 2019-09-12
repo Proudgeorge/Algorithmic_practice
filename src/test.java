2571d7046b0ff46f89a23c20867670a63170af72
Proudgeorge <zihaochen118@163.com>
2019-09-12 22:37:45 +0800

    剑指offer Java实现 不定时更新

--- src/test.java
+++ src/test.java
@@ -2,6 +2,7 @@ import org.junit.Test;
 
 import java.awt.*;
 import java.awt.geom.RoundRectangle2D;
+import java.util.Arrays;
 
 /**
  * @author simba@onlying.cn
@@ -9,21 +10,32 @@ import java.awt.geom.RoundRectangle2D;
  */
 public class test {
     public static void main(String[] args) {
-        final double a = 0.5;
-        double[] b = new double[8];
-
-        double[] c = new double[8];
-        for (int i = 0; i < 8; i++) {
-            b[i] = Math.random();
-            c[i] = Math.random();
-            System.out.println(i+"=====");
-            if (b[i]>a&&c[i]>a){
-                System.out.println(i);
-            }
-
+        int[] pre = {3,6,8,3,45,77,33,2,1};
+        for (int i=0;i<pre.length/2;i++){
+            swap(pre,i,pre.length-1-i);
         }
+        System.out.println(Arrays.toString(pre));
+//        final double a = 0.5;
+//        double[] b = new double[8];
+//
+//        double[] c = new double[8];
+//        for (int i = 0; i < 8; i++) {
+//            b[i] = Math.random();
+//            c[i] = Math.random();
+//            System.out.println(i+"=====");
+//            if (b[i]>a&&c[i]>a){
+//                System.out.println(i);
+//            }
+//
+//        }
+
     }
 
 
+    public static void swap(int[] str, int a, int b){
+        int temp = str[a];
+        str[a] = str[b];
+        str[b] = temp;
+    }
 
 }
